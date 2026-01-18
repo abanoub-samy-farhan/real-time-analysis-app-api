7d687c36cfee323029f1df55c6e9e387513a9beb
Abanoub Aziz <abanoubsamy2341@gmail.com>
2026-01-18 13:59:37 +0200

    Batching the api with RabbitMQ and MongoDB

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require("dotenv");
+import express from 'express';
+import cors from 'cors';
+import dotenv  from "dotenv";
+import morgan  from "morgan";
+import cookieParser from 'cookie-parser';
+
+import {connectDB} from "./config/db.js";
+
+import userRouter from './controllers/user.controller.js';
+import reviewRouter from './controllers/review.controller.js';
+import authMiddleware from './middlewares/authMiddleware.js';
 
 dotenv.config();
-
 const port = process.env.PORT || 3000;
 
-const { generateSummary } = require('./controllers/postData');
+// Connect to Database
+await connectDB();
 
 const app = express();
 app.use(cors({
-    origin: '*'
+    origin: 
+    [
+        'http://localhost:5173', 
+        'chrome-extension://hnkmhkmhdddfbhakmbgogilnggjjfiio'
+    ],
+    credentials: true,
 }));
 app.use(express.json());
-
+app.use(cookieParser());
+app.use(morgan("dev"));
 app.get('/', (req, res) => {
     res.send('Welcome to the Text Summarization API');
 })
 
-app.post('/generate', generateSummary);
+app.use('/users', userRouter);
+app.use('/reviews', authMiddleware, reviewRouter);
 
-module.exports = app;
+app.listen(port, () => {
+    console.log("Server listening on port %s", port);
+})
